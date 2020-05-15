d38e777acf4950e94216607bd9b2bd386e1e053e
diego59x <diegoalvarez1237@hotmail.com>
2020-05-14 23:15:48 -0600

    juego

---
+++ AlvarezDiego_Tarea7.1/Assets/Scripts/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private bool moverse = true;
+    
+    public GameObject waypoint1;
+    public GameObject waypoint2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (agent.remainingDistance < 0.05f)
+        {
+            if (moverse)
+            {
+                moverse = false;
+                agent.SetDestination(waypoint1.transform.position);
+            }
+            else
+            {
+                moverse = true;
+                agent.SetDestination(waypoint2.transform.position);
+            }
+        }
+            
+
+
+
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Si la pelota colsiona con las monedas, las monedas se destruyen
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ AlvarezDiego_Tarea7.1/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Movimiento : MonoBehaviour
+{
+	
+	private NavMeshAgent agent;
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			RaycastHit hitInfo;
+
+			if (Physics.Raycast(myRay, out hitInfo))
+			{
+				agent.SetDestination(hitInfo.point);
+			}
+		}
+	}
+}
